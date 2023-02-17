b29b949ecf353a23b2e4ea31335832048d0734cb
quangtuan1209 <125664628+quangtuan1209@users.noreply.github.com>
2023-02-17 22:18:10 +0700

    Update and rename README.md to NguyenQuangTuan_App.java

---
+++ NguyenQuangTuan_App.java
@@ -0,0 +1,7 @@
+public class NguyenQuangTuan_App { 
+{
+   public static void main(String[] args)
+   {
+      System.out.println("NguyenQuangTuan hoc java");
+      }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# NguyenQuangTuan_project-
