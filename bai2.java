fa791126003c293cdd657eab7c82b2da5cc0bdab
quangtuan1209 <125664628+quangtuan1209@users.noreply.github.com>
2023-03-07 19:12:24 +0700

    Create bai2.java

---
+++ bai2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class bai2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int a;
+		System.out.println("Nhap so nguyen a :");
+		a = sc.nextInt();
+	  if( a%2 == 0 ) {
+			System.out.println("a la so chan ");
+		}
+    else {
+			System.out.println("a la so le ");
+			}
+	}
